6b799d965525f0f42fca7bf821586a419f8b8da0
Juan Romo <juanromocode@gmail.com>
2019-02-13 00:34:17 -0600

    Add problem 16

---
+++ src/Problem016.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Problem016 {
+
+    public static void main(String[] args) {
+        BigInteger number = BigInteger.TWO.pow(1_000);
+        int sum = 0;
+        for (String digit : number.toString().split("")) {
+            sum += Integer.parseInt(digit);
+        }
+        System.out.println(sum);
+    }
+}
